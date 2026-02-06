fa7304e9cc5dff296a45817c086c11b63e5e37cb
KUp-2025 <kusaagra.upadhyay@gmail.com>
2026-02-07 00:31:19 +0530

    backend: initial duplicate check endpoint

--- dds/downloads/models.py
+++ dds/downloads/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class DownloadFile(models.Model):
+    filename = models.CharField(max_length=255)
+    file_size = models.BigIntegerField()
+    mime_type = models.CharField(max_length=100)
+    file_hash = models.CharField(max_length=64, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.filename
